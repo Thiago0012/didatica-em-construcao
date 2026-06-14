753123606802146afca417fd05190de818baca48
Thiago0012 <140083875+Thiago0012@users.noreply.github.com>
2026-06-14 20:14:35 -0300

    Conectar mural de comentarios online

--- comentarios-config.js
+++ comentarios-config.js
@@ -1,1 +1,1 @@
-window.DIDATICA_COMENTARIOS_ENDPOINT = "";
+window.DIDATICA_COMENTARIOS_ENDPOINT = "https://script.google.com/macros/s/AKfycby8CSs2ea1nPeCG-IpuMUHSjWK5lDwYO2xos4K2pVs2CzUiCK6Za4dVQoGvOJAxo9TW/exec";
